fc615613dab74528ca3979f5217212836a1cea03
mb_kondou <kondou@maboroshi.biz>
2016-02-08 15:37:56 +0900

    add .zshenv

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
