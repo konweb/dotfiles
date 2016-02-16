bde46f3463db82a9f02365939181fdf2ed915f97
mb_kondou <kondou@maboroshi.biz>
2016-02-16 10:48:24 +0900

    Add .tigrc files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
